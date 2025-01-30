15e0a75cdac80873366abf5e39da4b5d42f13911
bicycling <laxmi.joshibicycling2025@gmail.com>
2025-01-30 12:34:28 +0545

    cyclinghub
    Portfolio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+function learnMore() {
+    alert("Thank you for your interest! Stay tuned for more updates about my cycling adventures.");
+}
